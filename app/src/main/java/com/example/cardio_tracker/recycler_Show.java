2e9c3d3c20b109ca7aa30855af24e751b5ff4fdf
Momin1807011 <rahman1807011@stud.kuet.ac.bd>
2022-06-28 23:18:19 +0600

    Upload Flash Screen

---
+++ app/src/main/java/com/example/cardio_tracker/recycler_Show.java
@@ -0,0 +1,14 @@
+package com.example.cardio_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recycler_Show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_show);
+    }
+}
---
+++ app/src/main/java/com/example/cardio_tracker/splash_screen.java
@@ -0,0 +1,23 @@
+package com.example.cardio_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler=new Handler();
+        handler.postDelayed(() -> {
+            Intent intent=new Intent(splash_screen.this,recycler_Show.class);
+            startActivity(intent);
+            finish();
+        },3000);
+    }
+}
